79d8bafcefbcede400b55a36372bfbfc4a71904f
j3nsch <j3nsch@gmail.com>
2018-04-05 17:52:31 +0200

    OPUSVIER-3882 Use namespace for Opus_Bootstrap_Base. Remove database.

--- src/Bootstrap/Base.php
+++ src/Bootstrap/Base.php
@@ -28,9 +28,11 @@
  * @package     Opus_Bootstrap
  * @author      Ralf Claussnitzer (ralf.claussnitzer@slub-dresden.de)
  * @author      Jens Schwidder <schwidder@zib.de>
- * @copyright   Copyright (c) 2008-2016, OPUS 4 development team
+ * @copyright   Copyright (c) 2008-2018, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
+
+namespace Opus\Bootstrap;
 
 /**
  * Provide basic workflow of setting up an application.
@@ -39,14 +41,16 @@
  * @package     Opus_Bootstrap
  *
  */
-class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
+class Base extends \Zend_Application_Bootstrap_Bootstrap
+{
 
     /**
      * Override this to do custom backend setup.
      *
      * @return void
      */
-    protected function _initBackend() {
+    protected function _initBackend()
+    {
         $this->bootstrap(array('ZendCache', 'OpusLocale', 'Database', 'Logging'));
     }
 
@@ -54,11 +58,12 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
      * Initializes the location for temporary files.
      *
      */
-    protected function _initTemp() {
+    protected function _initTemp()
+    {
         $this->bootstrap('Configuration');
         $config = $this->getResource('Configuration');
         $tempDirectory = $config->workspacePath . '/tmp/';
-        Zend_Registry::set('temp_dir', $tempDirectory);
+        \Zend_Registry::set('temp_dir', $tempDirectory);
     }
 
     /**
@@ -66,7 +71,8 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
      *
      * @return void
      */
-    protected function _initZendCache() {
+    protected function _initZendCache()
+    {
         $this->bootstrap('Configuration');
         $config = $this->getResource('Configuration');
 
@@ -81,84 +87,14 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
             'cache_dir' => $config->workspacePath . '/cache/'
         );
 
-        $cache = Zend_Cache::factory('Core', 'File', $frontendOptions, $backendOptions);
+        $cache = \Zend_Cache::factory('Core', 'File', $frontendOptions, $backendOptions);
 
-        Zend_Translate::setCache($cache);
-        Zend_Locale::setCache($cache);
-        Zend_Locale_Data::setCache($cache);
-        Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
+        \Zend_Translate::setCache($cache);
+        \Zend_Locale::setCache($cache);
+        \Zend_Locale_Data::setCache($cache);
+        \Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
 
         return $cache;
-    }
-
-    /**
-     * Setup a database connection and store the adapter in the registry.
-     *
-     * @return void
-     *
-     * TODO put into configuration file (custom DB adapter)
-     */
-    protected function _initDatabase() {
-        $this->bootstrap(array('ZendCache', 'Logging','Configuration'));
-
-        $logger = $this->getResource('Logging');
-        $logger->debug('Initializing database.');
-
-        // use custom DB adapter
-        $config = new Zend_Config(array(
-            'db' => array(
-                'adapter' => 'Pdo_Mysqlutf8',
-                'params' => array(
-                    'adapterNamespace' => 'Opus_Db_Adapter'))), true);
-
-        // Include the above made configuration changes in the application configuration.
-        $config->merge(Zend_Registry::get('Zend_Config'));
-
-        // Put manipulated database configuration back to registry.
-        Zend_Registry::set('Zend_Config', $config);
-
-        // Use zend_Db factory to create a database adapter
-        // and make it the default for all tables.
-        $db = null;
-
-        try {
-            $db = Zend_Db::factory($config->db);
-            Zend_Db_Table::setDefaultAdapter($db);
-
-            // Register the adapter within Zend_Registry.
-            Zend_Registry::set('db_adapter', $db);
-        }
-        catch (Zend_Db_Adapter_Exception $e) {
-            $logger->err($e);
-            throw new Exception('OPUS Bootstrap Error: Could not connect to database.');
-        }
-
-        // Check database version
-        if (!Zend_Registry::isRegistered('opus.disableDatabaseVersionCheck') ||
-            !Zend_Registry::get('opus.disableDatabaseVersionCheck')) {
-            try {
-                $query = $db->query('SELECT version FROM schema_version');
-
-                $result = $query->fetch();
-
-                if (is_array($result) && array_key_exists('version', $result)) {
-                    $version = $result['version'];
-                    $expectedVersion = Opus_Version::getSchemaVersion();
-
-                    if ($version !== $expectedVersion) {
-                        throw new Exception("Database version '$version' does not match required '$expectedVersion'.");
-                    }
-                }
-                else {
-                    throw new Exception(
-                        'No database schema version found. Database is probably too old. Please update.'
-                    );
-                }
-            }
-            catch (Zend_Db_Statement_Exception $e) {
-                throw new Exception('Database schema is too old. Please update database.');
-            }
-        }
     }
 
     /**
@@ -176,9 +112,10 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
      * @return Zend_Config
      *
      */
-    protected function _initConfiguration() {
-        $config = new Zend_Config($this->getOptions());
-        Zend_Registry::set('Zend_Config', $config);
+    protected function _initConfiguration()
+    {
+        $config = new \Zend_Config($this->getOptions());
+        \Zend_Registry::set('Zend_Config', $config);
 
         return $config;
     }
@@ -212,7 +149,7 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
         $logfilePath = $config->workspacePath . '/log/' . $logFilename;
 
         $logfile = @fopen($logfilePath, 'a', false);
-        
+
         if ( $logfile === false ) {
             $path = dirname($logfilePath);
 
@@ -227,12 +164,12 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
         $GLOBALS['id_string'] = uniqid(); // Write ID string to global variables, so we can identify/match individual runs.
 
         $format = '%timestamp% %priorityName% (%priority%, ID '.$GLOBALS['id_string'].'): %message%' . PHP_EOL;
-        $formatter = new Zend_Log_Formatter_Simple($format);
+        $formatter = new \Zend_Log_Formatter_Simple($format);
 
-        $writer = new Zend_Log_Writer_Stream($logfile);
+        $writer = new \Zend_Log_Writer_Stream($logfile);
         $writer->setFormatter($formatter);
 
-        $logger = new Zend_Log($writer);
+        $logger = new \Zend_Log($writer);
         $logLevelName = 'INFO';
         $logLevelNotConfigured = false;
 
@@ -243,7 +180,7 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
             $logLevelNotConfigured = true;
         }
 
-        $zendLogRefl = new ReflectionClass('Zend_Log');
+        $zendLogRefl = new \ReflectionClass('Zend_Log');
 
         $invalidLogLevel = false;
 
@@ -255,8 +192,8 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
         }
 
         // filter log output
-        $priorityFilter = new Zend_Log_Filter_Priority($logLevel);
-        Zend_Registry::set('LOG_LEVEL', $logLevel);
+        $priorityFilter = new \Zend_Log_Filter_Priority($logLevel);
+        \Zend_Registry::set('LOG_LEVEL', $logLevel);
         $logger->addFilter($priorityFilter);
 
         if ($logLevelNotConfigured) {
@@ -268,7 +205,7 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
                     '\' configured.');
         }
 
-        Zend_Registry::set('Zend_Log', $logger);
+        \Zend_Registry::set('Zend_Log', $logger);
 
         $logger->debug('Logging initialized');
 
@@ -282,14 +219,14 @@ class Opus_Bootstrap_Base extends Zend_Application_Bootstrap_Bootstrap {
      *
      * @return void
      */
-    protected function _initOpusLocale() {
+    protected function _initOpusLocale()
+    {
         // Need cache initializatino for Zend_Locale.
         $this->bootstrap('ZendCache');
 
         // This avoids an exception if the locale cannot determined automatically.
         // TODO setup in config, still put in registry?
-        $locale = new Zend_Locale("de");
-        Zend_Registry::set('Zend_Locale', $locale);
+        $locale = new \Zend_Locale("de");
+        \Zend_Registry::set('Zend_Locale', $locale);
     }
-
 }
